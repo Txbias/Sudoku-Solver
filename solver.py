e09c1e93d03b745a6e4e74edd0638d224e418592
Tobias Hettler <Tobias-Hettler@protonmail.com>
2020-12-17 09:29:32 +0100

    Improved code-style

--- solver.py
+++ solver.py
@@ -1,19 +1,10 @@
-from gui import SudokuUI
-import time
-
-
-class Solver():
-    sudoku = []
-
+class Solver:
     def __init__(self, sudoku):
         self.sudoku = sudoku
 
     def is_solved(self):
         empty = self.find_all_empty()
-        if len(empty) == 0:
-            return True
-        else:
-            return False
+        return len(empty) == 0
 
     def find_all_empty(self):
         empty = list()
@@ -34,11 +25,8 @@ class Solver():
         # Check col
         for i in range(len(self.sudoku)):
             for j in range(len(self.sudoku[0])):
-                if i == pos[1]:
-                    if self.sudoku[j][i] == num:
-                        return False
-
-
+                if i == pos[1] and self.sudoku[j][i] == num:
+                    return False
 
         # Check box
         box_x = pos[0] // 3
@@ -55,7 +43,7 @@ class Solver():
             if i % 3 == 0 and i != 0:
                 print("-------------------------------------------")
 
-            for j in range(len(sudoku[0])):
+            for j in range(len(self.sudoku[0])):
                 if j % 3 == 0 and j != 0:
                     print("|", end="")
 
@@ -64,26 +52,25 @@ class Solver():
                 else:
                     print(str(self.sudoku[i][j]) + " ", end="")
 
-    def solve(self, draw_puzzle):
+    def solve(self):
         empty = self.find_all_empty()
         solved = []
         tested = {}
         found = False
 
         while not self.is_solved():
-
             pos = empty[len(solved)]
 
             for num in range(1, 10):
-                if not pos in tested:
+                if pos not in tested:
                     tested[pos] = []
-                if self.valid(num, pos) and not num in tested[pos]:
+                if self.valid(num, pos) and num not in tested[pos]:
                     found = True
                     self.sudoku[pos[0]][pos[1]] = num
                     solved.append(pos)
                     tested[pos].append(num)
                     break
-                elif not num in tested[pos]:
+                elif num not in tested[pos]:
                     tested[pos].append(num)
 
             if not found:
@@ -98,19 +85,3 @@ class Solver():
                     exit(0)
 
             found = False
-
-
-if __name__ == "__main__":
-    sudoku = [[6, 0, 5, 7, 0, 0, 1, 3, 2],
-               [7, 0, 0, 6, 0, 0, 5, 0, 8],
-               [0, 1, 9, 3, 0, 0, 0, 0, 4],
-               [0, 2, 0, 0, 0, 3, 0, 0, 0],
-               [0, 7, 3, 9, 0, 0, 2, 5, 0],
-               [0, 5, 1, 2, 0, 0, 0, 0, 9],
-               [5, 0, 8, 0, 0, 0, 0, 2, 0],
-               [0, 4, 0, 0, 7, 6, 9, 1, 5],
-               [0, 9, 0, 0, 4, 0, 6, 8, 0]]
-
-    sudoku_solver = Solver(sudoku)
-    sudoku_solver.solve(sudoku)
-    sudoku_solver.print_sudoku()
